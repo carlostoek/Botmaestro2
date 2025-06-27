1bf82de0d7f006b654b66fee991a48555a9339e7
carlostoek <carlostoek@gmail.com>
2025-06-27 13:14:55 -0600

    Register combinar pistas router

--- mybot/bot.py
+++ mybot/bot.py
@@ -25,6 +25,7 @@ from handlers import setup as setup_handlers # ¡IMPORTACIÓN CLAVE!
 from handlers.free_channel_admin import router as free_channel_admin_router
 from handlers.publication_test import router as publication_test_router
 import mochila
+import combinar_pistas
 
 from utils.config import BOT_TOKEN, VIP_CHANNEL_ID
 from services import (
@@ -97,6 +98,7 @@ async def main() -> None:
     dp.include_router(free_user.router)
     dp.include_router(lore_router)
     dp.include_router(mochila.router)
+    dp.include_router(combinar_pistas.router)
     dp.include_router(channel_access_router)
 
     # Tareas programadas
