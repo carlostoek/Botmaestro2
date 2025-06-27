eefe1b810eb0ca8bab5b8a1de117cf38758a8987
carlostoek <carlostoek@gmail.com>
2025-06-27 16:31:29 -0600

    Add lore piece editing flows

--- mybot/states/gamification_states.py
+++ mybot/states/gamification_states.py
@@ -9,3 +9,12 @@ class LorePieceAdminStates(StatesGroup):
     choosing_content_type = State()
     entering_text_content = State()
     uploading_file_content = State()
+
+    # States for editing an existing lore piece
+    editing_title = State()
+    editing_description = State()
+    editing_category = State()
+    editing_is_main_story = State()
+    editing_content_type = State()
+    editing_text_content = State()
+    editing_file_content = State()
