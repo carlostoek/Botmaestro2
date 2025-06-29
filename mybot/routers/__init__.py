c32182a8da8e3d9336c3b15ce62f870d361280d1
carlostoek <carlostoek@gmail.com>
2025-06-28 21:26:10 -0600

    Fix trivia router import

--- mybot/routers/__init__.py
+++ mybot/routers/__init__.py
@@ -1,2 +1,5 @@
 # No coloques mybot como módulo, es la raíz del proyecto
-from ..trivia_router import router as trivia_router
+# Importamos el router de trivia usando una ruta absoluta para evitar
+# problemas de importación cuando "routers" es tratado como un módulo de
+# primer nivel.
+from trivia_router import router as trivia_router
