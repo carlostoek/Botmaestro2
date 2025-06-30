4372877baa0d01a6413bb957fc17d8de94b8aa74
carlostoek <carlostoek@gmail.com>
2025-06-29 20:32:50 -0600

    add trivia analytics and integrations

--- mybot/trivia/__init__.py
+++ mybot/trivia/__init__.py
@@ -1,6 +1,8 @@
 from .schema import *
 from .manager import TriviaManager
 from .content import TriviaContentDelivery
+from .analytics import TriviaAnalytics
+from .integrations import TriviaPointsIntegration, unlock_storyboard_from_trivia
 
 __all__ = [
     "TriviaManager",
@@ -12,6 +14,9 @@ __all__ = [
     "TriviaSession",
     "TriviaTemplate",
     "TriviaContentDelivery",
+    "TriviaAnalytics",
+    "TriviaPointsIntegration",
+    "unlock_storyboard_from_trivia",
     "trivia_router",
 ]
 from .router import router as trivia_router
