012761915888fb117856818df465af7398642be0
carlostoek <carlostoek@gmail.com>
2025-07-06 13:26:40 -0600

    Update mission_service.py

--- mybot/services/mission_service.py
+++ mybot/services/mission_service.py
@@ -55,11 +55,7 @@ class MissionService:
 
     async def get_mission_by_id(self, mission_id: str) -> Mission | None:
         return await self.session.get(Mission, mission_id)
-    class Item:
-       pass
-
-   class UserItem:
-       pass
+    
       
     async def check_mission_completion_status(self, user: User, mission: Mission, target_message_id: int = None) -> tuple[bool, str]:
         """
